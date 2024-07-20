0204dd0c10b4b9de309a19ab9448f1a0c367637c
salman khan <salmankhan1920@protonmail.com>
2024-07-20 16:04:25 +0530

    deleted models, csv data

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Order, OrderLog
 
-# Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('order_id', 'user', 'id_name', 'date', 'timing', 'amount', 'cancel_status')
+    list_filter = ('cancel_status', 'date')
+    search_fields = ('order_id', 'id_name', 'user__username')
+    readonly_fields = ('timing', 'date')
+
+@admin.register(OrderLog)
+class OrderLogAdmin(admin.ModelAdmin):
+    list_display = ('user', 'order', 'action', 'timestamp')
+    list_filter = ('action', 'timestamp')
+    search_fields = ('user__username', 'order__order_id')
+    readonly_fields = ('timestamp',)
+
--- myapp/models.py
+++ myapp/models.py
@@ -1,24 +1,4 @@
 
-# from django.db import models
-
-# class Order(models.Model):
-#     CANCEL_CHOICES = [
-#         ('non-cancel', 'Non-Cancel'),
-#         ('cancel', 'Cancel'),
-#     ]
-
-#     id_name = models.CharField(max_length=50)
-#     timing = models.TimeField()
-#     date = models.DateField()
-#     order_id = models.CharField(max_length=50)
-#     amount = models.IntegerField()
-#     address_code = models.CharField(max_length=50)
-#     cancel_status = models.CharField(max_length=20, choices=CANCEL_CHOICES, default='non-cancel')
-
-#     def __str__(self):
-#         return f"Order {self.order_id}"
-
-
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -41,8 +21,8 @@ class Order(models.Model):
     cancel_status = models.CharField(max_length=20, choices=CANCEL_CHOICES, default='non-cancel')
 
 
-    def __str__(self):
-            return f"Order {self.order_id} by {self.user.username}"
+    # def __str__(self):
+    #         return f"Order {self.order_id} by {self.user.username}"
 
 
 
